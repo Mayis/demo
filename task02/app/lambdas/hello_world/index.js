9f8eca555d136725d42fc8fa955508fa97e4c919
Mayis <saroyan.mayis.ait@gmail.com>
2025-02-27 17:40:48 +0400

    fix

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,16 +1,23 @@
 exports.handler = async (event) => {
+  // Extract the HTTP method and path from the event object
   const httpMethod = event.requestContext.http.method;
   const path = event.requestContext.http.path;
 
   if (httpMethod === "GET" && path === "/hello") {
     return {
       statusCode: 200,
-      message: "Hello from Lambda",
+      body: JSON.stringify({
+        statusCode: 200,
+        message: "Hello from Lambda",
+      }),
     };
   } else {
     return {
       statusCode: 400,
-      message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
+      body: JSON.stringify({
+        statusCode: 400,
+        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
+      }),
     };
   }
 };
