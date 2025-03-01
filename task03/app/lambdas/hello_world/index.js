c2726cfa2788fc13f5149912061e3345102e18aa
Mayis <saroyan.mayis.ait@gmail.com>
2025-03-01 14:59:50 +0400

    feat: new ft

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,7 +1,8 @@
 exports.handler = async (event) => {
-  const response = {
-    statusCode: 200,
-    message: "Hello from Lambda",
-  };
-  return response;
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
 };
