8350d8a471d475aae2997146651b3a2865e7e710
Mayis <saroyan.mayis.ait@gmail.com>
2025-03-01 17:08:05 +0400

    new 2

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,7 +1,8 @@
 exports.handler = async (event) => {
+  // TODO implement
   const response = {
     statusCode: 200,
-    message: "Hello from Lambda",
+    message: "Hello from Lambda!",
   };
   return response;
 };
