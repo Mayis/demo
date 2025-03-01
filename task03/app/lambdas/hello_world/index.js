cab389667c1fb8fc255e69e484078a573fb33831
Mayis <saroyan.mayis.ait@gmail.com>
2025-03-01 16:02:24 +0400

    new ft

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,22 +1,8 @@
 exports.handler = async (event) => {
-  const httpMethod = event.requestContext.http.method;
-  const path = event.requestContext.http.path;
-
-  if (httpMethod === "GET" && path === "/hello") {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
+    // TODO implement
+    const response = {
         statusCode: 200,
-        message: "Hello from Lambda",
-      }),
+        body: JSON.stringify('Hello from Lambda!'),
     };
-  } else {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        statusCode: 400,
-        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
-      }),
-    };
-  }
+    return response;
 };
