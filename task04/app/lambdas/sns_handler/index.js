7746ee17642675b78fd28239556a8a10e5ab6fdf
Mayis <saroyan.mayis.ait@gmail.com>
2025-03-03 13:14:13 +0400

    remove task04

--- task04/app/lambdas/sns_handler/index.js
+++
@@ -1,4 +0,0 @@
-exports.handler = async (event) => {
-  console.log("Received SNS event:", JSON.stringify(event, null, 2));
-  return "Processed successfully";
-};
--- task04/app/lambdas/sqs_handler/index.js
+++
@@ -1,4 +0,0 @@
-exports.handler = async (event) => {
-  console.log("Received SQS event:", JSON.stringify(event, null, 2));
-  return "Processed successfully";
-};
