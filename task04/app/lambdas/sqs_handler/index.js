eb4c1f189b721ac796ef79a56ebf639d724ad20a
Mayis <saroyan.mayis.ait@gmail.com>
2025-03-03 15:31:38 +0400

    fix task04

--- task04/app/lambdas/sqs_handler/index.js
+++ task04/app/lambdas/sqs_handler/index.js
@@ -1,4 +1,8 @@
 exports.handler = async (event) => {
-  console.log("Received SQS event:", JSON.stringify(event, null, 2));
-  return "Processed successfully";
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
 };
