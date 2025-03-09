a0f40081e61c9cd5d1657bff68e6629dc3da9262
Mayis <saroyan.mayis.ait@gmail.com>
2025-03-09 20:33:54 +0400

    fix

--- task09/app/lambdas/api_handler/index.js
+++ task09/app/lambdas/api_handler/index.js
@@ -1,12 +1,12 @@
-const axios = require('axios');
+const axios = require("axios");
 
-exports.handler = async event => {
+exports.handler = async (event) => {
   const weatherApi =
-    'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m';
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
 
   const response = await axios.get(weatherApi);
 
-  if (event.requestContext.http.path === '/weather') {
+  if (event.requestContext.http.path === "/weather") {
     return JSON.stringify(response.data);
   }
 
@@ -15,7 +15,7 @@ exports.handler = async event => {
     body: JSON.stringify({
       statusCode: 400,
       message:
-        'Bad request syntax or unsupported method. Request path: /cmtr-7776e06b. HTTP method: GET',
+        "Bad request syntax or unsupported method. Request path: /cmtr-3e6647f0. HTTP method: GET",
     }),
   };
 };
